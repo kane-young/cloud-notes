b532126af147ab3584bcfe4e36b5bf39e7c4529d
kane-young <lyw2100@naver.com>
2021-06-14 17:58:44 +0900

    Feat : TextView 내의 Text 변화에 따른 데이터 저장을 위한 saveData() 메서드 구현

--- CloudNotes/CloudNotes/ViewModel/MemoDetailViewModel.swift
+++ CloudNotes/CloudNotes/ViewModel/MemoDetailViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 final class MemoDetailViewModel {
   private enum ContentConstant {
     static let emptyString = ""
-    static let doubleNewLine = "\n\n"
+    static let doubleNewLine = "\n"
   }
   
   private let context = MemoProvider.shared.context
@@ -28,6 +28,38 @@ final class MemoDetailViewModel {
   func configure(with memo: Memo) {
     self.memo = memo
     delegate?.changeMemo(content: content)
+  }
+  
+  func saveData(_ text: String) {
+    let seperatedText = seperateMemo(text)
+    guard let title = seperatedText?.title else { return }
+    guard let body = seperatedText?.body else { return }
+    memo.setValue(title, forKey: "title")
+    memo.setValue(body, forKey: "body")
+    memo.setValue(Int32(Date().timeIntervalSince1970), forKey: "lastModified")
+    do {
+      try self.context.save()
+    } catch {
+      fatalError(error.localizedDescription)
+    }
+  }
+  
+  private func seperateMemo(_ text: String) -> (title: String, body: String)? {
+    let seperatedText = text.split(separator: "\n", maxSplits: 1, omittingEmptySubsequences: true)
+    var title = ""
+    var body = ""
+    switch seperatedText.count {
+    case 0:
+      break
+    case 1:
+      title = String(seperatedText[0])
+    case 2:
+      title = String(seperatedText[0])
+      body = String(seperatedText[1])
+    default:
+      return nil
+    }
+    return (title, body)
   }
 }
 
